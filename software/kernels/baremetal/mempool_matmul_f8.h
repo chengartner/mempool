a8ba2281d2eccb6b5f4cdea14872af7c56b8e2be
sem25f14 Caroline Hengartner (chengartner) <sem25f14@pisoc3.ee.ethz.ch>
2025-04-08 11:32:19 +0200

    Add corrections to the matmul_f8 kernel Add missing semicolons in assembly code. Extend the shuffle-masks to 32 bits (instead of 16 bits) and rewrite as hexidecimal number. Add debug code (dump_try).

--- software/kernels/baremetal/mempool_matmul_f8.h
+++ software/kernels/baremetal/mempool_matmul_f8.h
@@ -13,24 +13,56 @@
 #include "builtins_v2.h"
 
 /*
-void matmul_4x2_parallel_inner_f8vec(const __fp8 *__restrict__ A,
+void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
                                 const __fp8 *__restrict__ B,
                                 __fp8 *__restrict__ C, uint32_t M,
                                 uint32_t N, uint32_t P, uint32_t core_id,
                                 uint32_t numThreads) {
-  v4b aElem00 = *(v4b *)&(A[0]);
-  v4b aElem01 = *(v4b *)&(A[4]);
-  v4b packed;
-  unsigned TempShuffle1 = 0b10001101;
+  
+  uint32_t i = 0; // loop counter for M
+  uint32_t j = 0; // loop counter for N
+  uint32_t k = 0; // loop counter for P
 
-  asm volatile(
-    "pv.pack %[packed], %[aElem01], %[aElem00];"
-    "pv.shuffle2.b %[packed], %[packed], %[TempShuffle1];"
-    : [aElem00] "+&r"(aElem00), [aElem01] "+&r"(aElem01)
-    : [packed] "r"(packed), [TempShuffle1] "r"(TempShuffle1)
-    );
+  for (k = core_id * 4; k < P; k += numThreads * 4) {
+    for (i = 0; i < M; i += 2) {
+      float volatile sum00 = 0.0f;
+      float volatile sum01 = 0.0f;
+      float volatile sum02 = 0.0f;
+      float volatile sum03 = 0.0f;
+      float volatile sum10 = 0.0f;
+      float volatile sum11 = 0.0f;
+      float volatile sum12 = 0.0f;
+      float volatile sum13 = 0.0f;
+      float volatile sumTemp = 0.0f;
+      for (j = 0; j < N; j += 4) {
+
+        v4b aVec0 = *(v4b *)&(A[i * N + j]);            // aVec0 = [a03 a02 a01 a00]
+        //dump_try(*(uint32_t*)&aVec0);
+        v4b aVec1 = *(v4b *)&(A[(i + 1) * N + j]);
+        v4b bVecTemp0 = *(v4b *)&(B[j * P + k]);        // bVecTemp0 = [b00 b01 b02 b03]
+        dump_try(*(uint32_t*)&bVecTemp0);
+        v4b bVecTemp1 = *(v4b *)&(B[(j + 1) * P + k]);  // bVecTemp1 = [b10 b11 b12 b13]
+        v4b bVecTemp2 = *(v4b *)&(B[(j + 2) * P + k]);  // bVecTemp2 = [b20 b21 b22 b23]
+        v4b bVecTemp3 = *(v4b *)&(B[(j + 3) * P + k]);  // bVecTemp3 = [b30 b31 b32 b33]
+        v4b bVec0, bVec1, bVec2, bVec3, bVecLow0, bVecLow1, bVecLow2, bVecLow3;
+        unsigned TempShuffle1 = 0x06040705; // [a b c d] => [b d a c]
+        unsigned TempShuffle2 = 0x05040706; // [a b c d] => [c d a b]
+        asm volatile(
+            "pv.pack.h %[bVec2], %[bVecTemp0], %[bVecTemp1];"    // bVec2 = [b02 b03 b12 b13]
+            "pv.shuffle2.b %[bVec2], %[bVec2], %[TempShuffle1];" // bVec2 = [b03 b13 b02 b12]
+            "pv.extract.h %[bVec3], %[bVec2], 1;" // bVec3 = [0 0 b03 b13]
+            //"pv.shuffle2.b %[bVec3], %[bVec3], %[TempShuffle2];" // bVec3 = [b03 b13 0 0]
+            "pv.extract.h %[bVec2], %[bVec2], 0;" // bVec2 = [0 0 b02 b12]
+            : [bVec2] "+&r"(bVec2), [bVec3] "+&r"(bVec3)
+            : [bVecTemp0] "r"(bVecTemp0), [bVecTemp1] "r"(bVecTemp1), [TempShuffle1] "r"(TempShuffle1));
+        dump_try(*(uint32_t*)&bVec3);
+        dump_try(*(uint32_t*)&bVec2);
+      }
+    }
+  }
 }
 */
+
 
 void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
                                 const __fp8 *__restrict__ B,
@@ -55,30 +87,32 @@ void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
       float volatile sumTemp = 0.0f;
       for (j = 0; j < N; j += 4) {
 
-        v4b aVec0 = *(v4b *)&(A[i * N + j]);
+        v4b aVec0 = *(v4b *)&(A[i * N + j]);            // aVec0 = [a03 a02 a01 a00]
+        //dump_try(*(uint32_t*)&aVec0);
         v4b aVec1 = *(v4b *)&(A[(i + 1) * N + j]);
         v4b bVecTemp0 = *(v4b *)&(B[j * P + k]);        // bVecTemp0 = [b00 b01 b02 b03]
+        //dump_try(*(uint32_t*)&bVecTemp0);
         v4b bVecTemp1 = *(v4b *)&(B[(j + 1) * P + k]);  // bVecTemp1 = [b10 b11 b12 b13]
         v4b bVecTemp2 = *(v4b *)&(B[(j + 2) * P + k]);  // bVecTemp2 = [b20 b21 b22 b23]
         v4b bVecTemp3 = *(v4b *)&(B[(j + 3) * P + k]);  // bVecTemp3 = [b30 b31 b32 b33]
         v4b bVec0, bVec1, bVec2, bVec3, bVecLow0, bVecLow1, bVecLow2, bVecLow3;
-        unsigned TempShuffle1 = 0b0110010001110101; // [a b c d] => [b d a c]
-        unsigned TempShuffle2 = 0b0101010001110110; // [a b c d] => [c d a b]
+        unsigned TempShuffle1 = 0x06040705; // [a b c d] => [b d a c]
+        unsigned TempShuffle2 = 0x05040706; // [a b c d] => [c d a b]
         asm volatile(
             "pv.pack.h %[bVec2], %[bVecTemp0], %[bVecTemp1];"    // bVec2 = [b02 b03 b12 b13]
             "pv.shuffle2.b %[bVec2], %[bVec2], %[TempShuffle1];" // bVec2 = [b03 b13 b02 b12]
             "pv.extract.h %[bVec3], %[bVec2], 1;" // bVec3 = [0 0 b03 b13]
-            "pv.shuffle2.b %[bVec3], %[bVec3], %[TempShuffle2]" // bVec3 = [b03 b13 0 0]
+            "pv.shuffle2.b %[bVec3], %[bVec3], %[TempShuffle2];" // bVec3 = [b03 b13 0 0]
             "pv.extract.h %[bVec2], %[bVec2], 0;" // bVec2 = [0 0 b02 b12]
-            "pv.shuffle2.b %[bVec2], %[bVec2], %[TempShuffle2]" // bVec2 = [b02 b12 0 0]            
+            "pv.shuffle2.b %[bVec2], %[bVec2], %[TempShuffle2];" // bVec2 = [b02 b12 0 0]            
             "pv.shuffle2.b %[bVecTemp0], %[bVecTemp0], %[TempShuffle2];" // bVecTemp0 = [b02 b03 b00 b01]
             "pv.shuffle2.b %[bVecTemp1], %[bVecTemp1], %[TempShuffle2];" // bVecTemp1 = [b12 b13 b10 b11]
             "pv.pack.h %[bVec0], %[bVecTemp0], %[bVecTemp1];"    // bVec0 = [b00 b01 b10 b11]
             "pv.shuffle2.b %[bVec0], %[bVec0], %[TempShuffle1];" // bVec0 = [b01 b11 b00 b10]
             "pv.extract.h %[bVec1], %[bVec0], 1;" // bVec1 = [0 0 b01 b11]
-            "pv.shuffle2.b %[bVec1], %[bVec1], %[TempShuffle2]" // bVec3 = [b01 b11 0 0]
+            "pv.shuffle2.b %[bVec1], %[bVec1], %[TempShuffle2];" // bVec3 = [b01 b11 0 0]
             "pv.extract.h %[bVec0], %[bVec0], 0;" // bVec0 = [0 0 b00 b10]
-            "pv.shuffle2.b %[bVec0], %[bVec0], %[TempShuffle2]" // bVec0 = [b00 b10 0 0]
+            "pv.shuffle2.b %[bVec0], %[bVec0], %[TempShuffle2];" // bVec0 = [b00 b10 0 0]
 
             "pv.pack.h %[bVecLow2], %[bVecTemp2], %[bVecTemp3];"       // bVecLow2 = [b22 b23 b32 b33]
             "pv.shuffle2.b %[bVecLow2], %[bVecLow2], %[TempShuffle1];" // bVecLow2 = [b23 b33 b22 b32]
@@ -90,7 +124,15 @@ void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
             "pv.shuffle2.b %[bVecLow0], %[bVecLow0], %[TempShuffle1];" // bVecLow0 = [b21 b31 b20 b30]
             "pv.extract.h %[bVecLow1], %[bVecLow0], 1;" // bVecLow1 = [b21 b31]
             "pv.extract.h %[bVecLow0], %[bVecLow0], 0;" // bVecLow0 = [b20 b30]
+            : [bVec0] "+&r"(bVec0), [bVec1] "+&r"(bVec1), [bVec2] "+&r"(bVec2), [bVec3] "+&r"(bVec3),
+              [bVecLow0] "+&r"(bVecLow0), [bVecLow1] "+&r"(bVecLow1), [bVecLow2] "+&r"(bVecLow2), [bVecLow3] "+&r"(bVecLow3),
+              [bVecTemp0] "+&r"(bVecTemp0), [bVecTemp1] "+&r"(bVecTemp1), [bVecTemp2] "+&r"(bVecTemp2),
+              [bVecTemp3] "+&r"(bVecTemp3)
+            : [aVec0] "r"(aVec0), [aVec1] "r"(aVec1), 
+              [TempShuffle1] "r"(TempShuffle1), [TempShuffle2] "r"(TempShuffle2));
 
+        dump_try(*(uint32_t*)&bVec1);
+        asm volatile(
             "vfdotpexa.s.b %[sum00], %[aVec0], %[bVec0];"      // aRes0 = a00*b00 + a01*b10
             "vfdotpexb.s.b %[sumTemp], %[aVec0], %[bVecLow0];" // aRes1 = a02*b20 + a03*b30
             "vfadd.h %[sum00], %[sum00], %[sumTemp];"
@@ -117,12 +159,10 @@ void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
             "vfadd.h %[sum13], %[sum03], %[sumTemp];"
             : [sum00] "=&r"(sum00), [sum01] "=&r"(sum01), [sum02] "=&r"(sum02), [sum03] "=&r"(sum03),
               [sum10] "=&r"(sum10), [sum11] "=&r"(sum11), [sum12] "=&r"(sum12), [sum13] "=&r"(sum13),
-              [bVec0] "+&r"(bVec0), [bVec1] "+&r"(bVec1), [bVec2] "+&r"(bVec2), [bVec3] "+&r"(bVec3),
-              [bVecLow0] "+&r"(bVecLow0), [bVecLow1] "+&r"(bVecLow1), [bVecLow2] "+&r"(bVecLow2), [bVecLow3] "+&r"(bVecLow3),
-              [bVecTemp0] "+&r"(bVecTemp0), [bVecTemp1] "+&r"(bVecTemp1), [bVecTemp2] "+&r"(bVecTemp2),
-              [bVecTemp3] "+&r"(bVecTemp3), [sumTemp] "+&r"(sumTemp)
+              [sumTemp] "+&r"(sumTemp)
             : [aVec0] "r"(aVec0), [aVec1] "r"(aVec1), 
-              [TempShuffle1] "r"(TempShuffle1), [TempShuffle2] "r"(TempShuffle2));
+              [bVec0] "r"(bVec0), [bVec1] "r"(bVec1), [bVec2] "r"(bVec2), [bVec3] "r"(bVec3),
+              [bVecLow0] "r"(bVecLow0), [bVecLow1] "r"(bVecLow1), [bVecLow2] "r"(bVecLow2), [bVecLow3] "r"(bVecLow3));
       }
       v4b res0, res1;
       asm volatile("vfcpka.b.s %[res0], %[sum03], %[sum02];"
@@ -137,6 +177,7 @@ void matmul_2x4_parallel_inner_f8vec(const __fp8 *__restrict__ A,
     }
   }
 }
+
 
 /*
 void matmul_4x2_parallel_inner_f8vec(const __fp8 *__restrict__ A,
